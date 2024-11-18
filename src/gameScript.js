160ed5e8727dcf492993b449db25838c8310ea15
dominikDurinic <dd53270@fer.hr>
2024-11-18 01:53:51 +0100

    Fix collision handling and clear canvas

--- src/gameScript.js
+++ src/gameScript.js
@@ -1,5 +1,5 @@
-var canvas = document.getElementById("gameCanvas");
-var canvCtx = canvas.getContext("2d");
+let canvas = document.getElementById("gameCanvas");
+let canvCtx = canvas.getContext("2d");
 
 //postavljamo dimenzije canvasa na dimenzije css
 //kako bi odgovarale veličine prikaza objekata u canvasu
@@ -22,7 +22,8 @@ const canvasHeight = canvas.height;
 //cigla
 const brickW = canvasWidth / 10;
 const brickH = 50;
-const yOffset = 150;
+const yOffset = 130;
+let bricks = [];
 
 //palica
 const stickWidth = 300;
@@ -37,43 +38,48 @@ let ballY = canvasHeight - 100; //inicijalna y koordinata loptice
 let prevBallX = ballX;
 let prevBallY = ballY;
 
-canvCtx.border = "1px solid black";
-
 let newCoordinateX = 0;
 let newCoordinateY = 0;
 
 function randomColor() {
+  //slucajni odabir boje cigle
   let index = Math.floor(Math.random() * brickColors.length);
-  return brickColors[index];
+  return index;
 }
 
 //naslov igre
-canvCtx.fillStyle = "black";
-canvCtx.font = "bold 40px Verdana";
-canvCtx.fillText("BRICK DOWN", (canvasWidth - 300) / 2, 60);
-canvCtx.font = "bold 20px Verdana";
-canvCtx.fillText("online arcade game", (canvasWidth - 210) / 2, 85);
-
-canvCtx.shadowBlur = 20;
-canvCtx.shadowColor = "gray";
+function drawText() {
+  //iscrtavanje teksta naslov i bodovi
+  canvCtx.fillStyle = "black";
+  canvCtx.font = "bold 40px Verdana";
+  canvCtx.fillText("BRICK DOWN", (canvasWidth - 300) / 2, 60);
+  canvCtx.font = "bold 20px Verdana";
+  canvCtx.fillText("online arcade game", (canvasWidth - 210) / 2, 85);
+  canvCtx.fillStyle = "black";
+  canvCtx.font = "bold 20px Verdana";
+  canvCtx.fillText("Score: ", canvasWidth - 200, 60);
+  canvCtx.font = "bold 20px Verdana";
+  canvCtx.fillText("Best Score: ", canvasWidth - 256, 85);
+}
 
 //po redovima i stupcima iscrtava pravokutnike različitih boja
 for (let j = 0; j < 5; j++) {
   newCoordinateY = j * brickH + yOffset;
 
   for (let i = 0; i < 10; i++) {
-    canvCtx.fillStyle = randomColor();
+    let ind = randomColor();
+    canvCtx.fillStyle = brickColors[ind];
     newCoordinateX = i * brickW;
+    canvCtx.shadowBlur = 20;
+    canvCtx.shadowColor = "gray";
     canvCtx.fillRect(newCoordinateX, newCoordinateY, brickW, brickH);
+    bricks.push({ xPos: newCoordinateX, yPos: newCoordinateY, color_ind: ind }); //spremamo cigle zbog detekcije sudara loptice i pojedine cigle
   }
 }
 
 function drawStick(x) {
   //funkcija za iscrtavanje palice s obzirom na pomak
-  canvCtx.shadowBlur = 0;
-  canvCtx.shadowColor = "rgba(0, 0, 0, 0)";
-  canvCtx.clearRect(stickX, stickY, stickWidth + 5, stickHeight); //brisanje stare palice
-
+  clearCanvas();
   //provjera je li palica na krajevima canvasa
   if (x < 0) {
     stickX = 0;
@@ -86,6 +92,7 @@ function drawStick(x) {
   //pravokutna palica
   canvCtx.fillStyle = "black";
   canvCtx.shadowBlur = 10;
+  canvCtx.shadowColor = "gray";
   canvCtx.fillRect(stickX, stickY, stickWidth, stickHeight); //crtanjenove palice
 }
 
@@ -109,23 +116,32 @@ let radius = 15;
 
 function drawBall() {
   //funkcija za iscrtavanje loptice na određenoj poziciji
-  canvCtx.shadowBlur = 0;
-  canvCtx.shadowColor = "rgba(0, 0, 0, 0)";
-  canvCtx.clearRect(
-    prevBallX - radius - 1,
-    prevBallY - radius - 1,
-    2 * radius + 2,
-    2 * radius + 2
-  );
+  clearCanvas();
+  drawStick(stickX);
   canvCtx.beginPath();
   canvCtx.arc(ballX, ballY, radius, 0, 2 * Math.PI, false);
-  canvCtx.fillStyle = "black";
   canvCtx.shadowBlur = 10;
+  canvCtx.shadowColor = "gray";
   canvCtx.fillStyle = "red";
   canvCtx.fill();
   canvCtx.closePath();
   prevBallX = ballX;
   prevBallY = ballY;
+}
+
+function drawBricks() {
+  bricks.forEach((brick) => {
+    canvCtx.fillStyle = brickColors[brick.color_ind];
+    canvCtx.shadowBlur = 10;
+    canvCtx.shadowColor = "gray";
+    canvCtx.fillRect(brick.xPos, brick.yPos, brickW, brickH); //crtanje cigli
+  });
+}
+
+function clearCanvas() {
+  canvCtx.clearRect(0, 0, canvasWidth, canvasHeight);
+  drawBricks();
+  drawText();
 }
 
 function updatePosition() {
@@ -136,20 +152,41 @@ function updatePosition() {
   if (ballX + radius > canvas.width || ballX - radius < 0) {
     move_x = -move_x;
   }
-  if (ballY + radius > canvas.height || ballY - radius < 0) {
+  if (ballY + radius > canvas.height || ballY - radius < yOffset) {
     move_y = -move_y;
   }
   //provjera sudara loptice i palice
   if (
-    ballX + radius + 1 >= stickX &&
+    ballX + radius >= stickX &&
     ballX - radius <= stickX + stickWidth &&
-    ballY + radius + 1 >= stickY &&
+    ballY + radius >= stickY &&
     ballY - radius <= stickY + stickHeight
   ) {
     if (ballX > stickX && ballX < stickX + stickWidth) {
       move_y = -move_y; // promjena smjera loptice pri sudaru s palicom
+    } else {
+      move_x = -move_x;
     }
   }
+
+  //provjera sudara loptice i pojedine cigle
+  bricks.forEach((brick, index) => {
+    if (
+      ballX + radius >= brick.xPos &&
+      ballX - radius <= brick.xPos + brickW &&
+      ballY + radius >= brick.yPos &&
+      ballY - radius <= brick.yPos + brickH
+    ) {
+      if (ballX > brick.xPos && ballX < brick.xPos + brickW) {
+        move_y = -move_y; //promjena smjera y pozicije loptice pri sudaru s ciglom, vraća se u suprotnom smjeru
+      } else {
+        move_x = -move_x; //promjena smjera x pozicije loptice pri sudaru s ciglom
+      }
+      bricks.splice(index, 1); //brisanje sudarene cigle iz niza
+      clearCanvas();
+      canvCtx.clearRect(brick.xPos, brick.yPos, brickW, brickH); //brisanje sudarene cigle
+    }
+  });
 }
 
 function animate() {
